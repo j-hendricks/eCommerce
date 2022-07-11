167c8608c6339e8adf2f194360b9c3ae004e8d45
John Hendricks <jwh8zc@virginia.edu>
2022-07-10 22:50:45 -0400

    Added further analysis of data

--- Queries/data_exploration.sql
+++ Queries/data_exploration.sql
@@ -74,3 +74,84 @@ SELECT country, AVG(meanproductssold) as "Average"
 FROM gender_and_country
 GROUP BY country
 ORDER BY "Average" DESC;
+
+----------------------
+
+SELECT * FROM country
+
+SELECT *
+FROM country
+WHERE buyers > 100
+ORDER BY topbuyerratio DESC 
+LIMIT 10;
+
+SELECT * FROM country;
+
+SELECT country, buyers, (100* buyers / (SELECT SUM(buyers) as "Total Buyers" FROM country)) as "Proportion of Buyers (%)"
+FROM country
+ORDER BY "Proportion of Buyers (%)" DESC;
+
+SELECT country, (100* topbuyers / (SELECT SUM(topbuyers) FROM country)) as "Proportion of Top Buyers (%)"
+FROM country
+ORDER BY "Proportion of Top Buyers (%)" DESC;
+
+-- top buyers were defined by those having bought 10 or more products
+SELECT country, (100* buyers / (SELECT SUM(buyers) as "Total Buyers" FROM country)) as "Proportion of Buyers (%)", (100* topbuyers / (SELECT SUM(topbuyers) FROM country)) as "Proportion of Top Buyers (%)"
+FROM country
+ORDER BY "Proportion of Buyers (%)" DESC
+WHERE ("Proportion of Buyers (%)") AND ("Proportion of Top Buyers (%)");
+
+SELECT country, meanproductsbought
+FROM country
+ORDER BY meanproductsbought DESC;
+
+DROP TABLE product_summary;
+
+SELECT 
+	SUM(totalproductsbought) as "Total Products Bought", 
+	SUM(totalproductswished) as "Total Products Wished", 
+	SUM(totalproductsliked) as "Total Products Liked",
+	(100 * (SUM(totalproductsbought)) / (SUM(totalproductswished)))
+		as "Percent Bought From Wish List (%)",
+	(100 * (SUM(totalproductsbought)) / (SUM(totalproductsliked)))
+		as "Percent Bought From Liked Products (%)"
+	INTO product_summary
+	FROM country
+	ORDER BY "Total Products Bought" DESC;
+
+SELECT * FROM product_summary;
+
+SELECT * FROM customer;
+
+DROP TABLE sellers;
+
+SELECT DISTINCT identifierhash,
+country,
+productslisted,
+productssold,
+productspassrate,
+gender,
+hasprofilepicture,
+dayssincelastlogin
+INTO sellers
+from customer
+WHERE productslisted > 0;
+
+SELECT COUNT(identifierhash)
+FROM sellers;
+
+SELECT COUNT(identifierhash)
+FROM customer;
+
+SELECT * FROM sellers;
+
+SELECT gender, ROUND(AVG(productspassrate)) as "Average Pass Rate (%)"
+FROM sellers
+WHERE productssold > 10
+GROUP BY gender;
+
+SELECT hasprofilepicture, ROUND(AVG(productspassrate)) as "Average Pass Rate (%)", SUM(productssold) as "Total Products Sold"
+FROM sellers
+WHERE productssold > 10
+GROUP BY hasprofilepicture;
+
